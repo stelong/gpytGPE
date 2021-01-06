05405ad5b8d1b3235792a2fcfee2f5c876a9570d
Stefano Longobardi <sl18@bioeng306-lap.isd.kcl.ac.uk>
2021-01-06 16:53:56 +0100

    added useful examples

---
+++ example-scripts/1_emulation_step_by_step.py
@@ -0,0 +1,152 @@
+import random
+import sys
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from sklearn.model_selection import train_test_split
+
+from gpytGPE.gpe import GPEmul
+from gpytGPE.utils.design import read_labels
+from gpytGPE.utils.metrics import R2Score
+from gpytGPE.utils.plotting import plot_dataset
+
+LEARNING_RATE = 0.1
+MAX_EPOCHS = 1000
+METRIC = "R2Score"
+N_RESTARTS = 1
+PATIENCE = 20
+SEED = 8
+
+
+def main():
+    # ================================================================
+    # Making the code reproducible
+    # ================================================================
+    seed = SEED
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+
+    # ================================================================
+    # Loading and visualising dataset
+    # ================================================================
+    path_in = sys.argv[1].rstrip("/") + "/"
+    X = np.loadtxt(path_in + "X.txt", dtype=float)
+    Y = np.loadtxt(path_in + "Y.txt", dtype=float)
+
+    xlabels = read_labels(path_in + "xlabels.txt")
+    ylabels = read_labels(path_in + "ylabels.txt")
+    plot_dataset(X, Y, xlabels, ylabels)
+
+    # ================================================================
+    # Building example training and validation datasets
+    # ================================================================
+    idx_feature = sys.argv[2]
+    print(f"\n{ylabels[int(idx_feature)]} feature selected for emulation.")
+
+    y = np.copy(Y[:, int(idx_feature)])
+
+    X_train, X_val, y_train, y_val = train_test_split(
+        X, y, test_size=0.2, random_state=seed
+    )
+
+    # ================================================================
+    # Training GPE
+    # ================================================================
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    lr = LEARNING_RATE
+    max_epochs = MAX_EPOCHS
+    metric = METRIC
+    n_restarts = N_RESTARTS
+    patience = PATIENCE
+
+    path_out = sys.argv[3].rstrip("/") + "/" + idx_feature + "/"
+    Path(path_out).mkdir(parents=True, exist_ok=True)
+
+    emul = GPEmul(X_train, y_train, device, learn_noise=False, scale_data=True)
+    emul.train(
+        X_val,
+        y_val,
+        n_restarts,
+        lr,
+        max_epochs,
+        patience,
+        savepath=path_out,
+        save_losses=False,
+        watch_metric=metric,
+    )
+
+    # ================================================================
+    # Saving trained GPE
+    # ================================================================
+    filename = "gpe.pth"
+    emul.save(filename=filename)
+
+    # ================================================================
+    # Loading already trained GPE
+    # ================================================================
+    # NOTE: you need exactely the same training dataset used previously
+    # ================================================================
+    emul = GPEmul.load(path_out, X_train, y_train, filename=filename)
+
+    # ================================================================
+    # Testing trained GPE at new input points (inference)
+    # ================================================================
+    # NOTE: we will use the validation dataset as an example
+    # ================================================================
+    X_test = X_val
+    y_test = y_val
+
+    y_pred_mean, y_pred_std = emul.predict(X_test)
+    r2s = R2Score(emul.tensorize(y_test), emul.tensorize(y_pred_mean))
+    print(f"\nAccuracy on testing dataset: R2Score = {r2s:.6f}.")
+
+    # ================================================================
+    # Plotting predictions vs observations
+    # ================================================================
+    height = 9.36111
+    width = 5.91667
+    fig, axis = plt.subplots(1, 1, figsize=(2 * width, 2 * height / 3))
+
+    l = np.argsort(
+        y_pred_mean
+    )  # let's sort predicted values for a better visualisation
+    ci = 3
+
+    axis.scatter(
+        np.arange(len(l)),
+        y_test[l],
+        facecolors="none",
+        edgecolors="C0",
+        label="observed",
+    )
+    axis.scatter(
+        np.arange(len(l)),
+        y_pred_mean[l],
+        facecolors="C0",
+        s=16,
+        label="predicted",
+    )
+    axis.errorbar(
+        np.arange(len(l)),
+        y_pred_mean[l],
+        yerr=ci * y_pred_std[l],
+        c="C0",
+        ls="none",
+        lw=0.5,
+        label=f"uncertainty ({ci} STD)",
+    )
+    axis.set_title(f"R2Score = {r2s:.6f}", fontsize=12)
+    axis.set_xticks([])
+    axis.set_xticklabels([])
+    axis.set_ylabel(ylabels[int(idx_feature)], fontsize=12)
+    axis.legend(loc="upper left")
+
+    fig.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ example-scripts/3_global_sobol_sensitivity_analysis.py
@@ -0,0 +1,112 @@
+import multiprocessing
+import random
+import sys
+from itertools import combinations
+
+import numpy as np
+import torch
+from SALib.analyze import sobol
+from SALib.sample import saltelli
+from scipy.special import binom
+
+from gpytGPE.gpe import GPEmul
+from gpytGPE.utils.design import get_minmax, read_labels
+from gpytGPE.utils.plotting import gsa_box, gsa_donut
+
+EMUL_TYPE = "full"  # possible choices are: 'full', 'best'
+METRIC = "R2Score"
+N = 100
+N_DRAWS = 1000
+SEED = 8
+THRE = 0.01
+
+
+def main():
+    # ================================================================
+    # Making the code reproducible
+    # ================================================================
+    seed = SEED
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    # ================================================================
+    # GPE loading
+    # ================================================================
+    emul_type = EMUL_TYPE
+    idx_feature = sys.argv[2]
+    metric = METRIC
+    path = sys.argv[1].rstrip("/") + "/" + idx_feature + "/"
+
+    if emul_type == "best":
+        metric_score_list = np.loadtxt(path + metric + "_cv.txt", dtype=float)
+        if metric == "R2Score":
+            best_split = np.argmax(metric_score_list)
+        else:
+            best_split = np.argmin(metric_score_list)
+        path = path + f"{best_split}/"
+
+    X_train = np.loadtxt(path + "X_train.txt", dtype=float)
+    y_train = np.loadtxt(path + "y_train.txt", dtype=float)
+    emul = GPEmul.load(path, X_train, y_train)
+
+    # ================================================================
+    # Estimating Sobol' sensitivity indices
+    # ================================================================
+    label = read_labels("data/ylabels.txt")
+
+    n = N
+    n_draws = N_DRAWS
+
+    D = X_train.shape[1]
+    I = get_minmax(X_train)
+
+    index_i = read_labels("data/xlabels.txt")
+    index_ij = [
+        "({}, {})".format(c[0], c[1]) for c in combinations(index_i, 2)
+    ]
+
+    problem = {"num_vars": D, "names": index_i, "bounds": I}
+
+    X_sobol = saltelli.sample(
+        problem, n, calc_second_order=True
+    )  # n x (2D + 2) | if calc_second_order == False --> n x (D + 2)
+    Y = emul.sample(X_sobol, n_draws=n_draws)
+
+    ST = np.zeros((0, D), dtype=float)
+    S1 = np.zeros((0, D), dtype=float)
+    S2 = np.zeros((0, int(binom(D, 2))), dtype=float)
+
+    for i in range(n_draws):
+        S = sobol.analyze(
+            problem,
+            Y[i],
+            calc_second_order=True,
+            parallel=True,
+            n_processors=multiprocessing.cpu_count(),
+            seed=seed,
+        )
+        total_order, first_order, (_, second_order) = sobol.Si_to_pandas_dict(
+            S
+        )
+
+        ST = np.vstack((ST, total_order["ST"].reshape(1, -1)))
+        S1 = np.vstack((S1, first_order["S1"].reshape(1, -1)))
+        S2 = np.vstack((S2, np.array(second_order["S2"]).reshape(1, -1)))
+
+    np.savetxt(path + "STi.txt", ST, fmt="%.6f")
+    np.savetxt(path + "Si.txt", S1, fmt="%.6f")
+    np.savetxt(path + "Sij.txt", S2, fmt="%.6f")
+
+    # ================================================================
+    # Plotting
+    # ================================================================
+    thre = THRE
+    ylab = label[int(idx_feature)]
+
+    gsa_donut(path, thre, index_i, ylab, savefig=True)
+    gsa_box(path, thre, index_i, index_ij, ylab, savefig=True)
+
+
+if __name__ == "__main__":
+    main()
