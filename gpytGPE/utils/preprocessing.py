3d8ee08e55940f77d3f1ac9a079506a60ced4811
Stefano Longobardi <sl18@bioeng306-lap.isd.kcl.ac.uk>
2021-03-01 16:36:14 +0100

    added log-transform of data

--- gpytGPE/utils/preprocessing.py
+++ gpytGPE/utils/preprocessing.py
@@ -1,28 +1,56 @@
 import numpy as np
 
 
-class Scaler:
-    def __init__(self):
-        self.mu = []
-        self.sigma = []
+class StandardScaler:
+    def __init__(self, log_transform=False):
+        self.log_transform = log_transform
+        self.mu = None
+        self.sigma = None
 
     def fit(self, X):
-        self.in_dim = X.shape[1]
-        for i in range(self.in_dim):
-            self.mu.append(np.mean(X[:, i]))
-            self.sigma.append(np.std(X[:, i]))
+        if self.log_transform:
+            X = np.log(X)
+        self.mu = np.mean(X, axis=0)
+        self.sigma = np.std(X, axis=0)
 
     def transform(self, X):
-        X_ = np.full_like(X, 0, dtype=float)
-        for i in range(self.in_dim):
-            X_[:, i] = (X[:, i] - self.mu[i]) / self.sigma[i]
+        if self.log_transform:
+            X = np.log(X)
+        X_ = (X - self.mu) / self.sigma
         return X_
 
-    def inverse_transform(self, X):
-        X_ = np.full_like(X, 0, dtype=float)
-        for i in range(self.in_dim):
-            X_[:, i] = self.sigma[i] * X[:, i] + self.mu[i]
+    def inverse_transform(self, X_):
+        X = self.sigma * X_ + self.mu
+        if self.log_transform:
+            X = np.exp(X)
+        return X
+
+    def inverse_transform_predictions(self, y_mean_, y_std_):
+        y_mean = self.sigma * y_mean_ + self.mu
+        y_std = self.sigma * y_std_
+        if self.log_transform:
+            return (
+                np.exp(y_mean + 0.5 * np.power(y_std, 2)),
+                (np.exp(np.power(y_std, 2)) - 1)
+                * np.exp(2 * y_mean + np.power(y_std, 2)),
+            )
+        else:
+            return y_mean, y_std
+
+
+class UnitCubeScaler:
+    def __init__(self):
+        self.a = None
+        self.b = None
+
+    def fit(self, X):
+        self.a = np.min(X, axis=0)
+        self.b = np.max(X, axis=0)
+
+    def transform(self, X):
+        X_ = (X - self.a) / (self.b - self.a)
         return X_
 
-    def inverse_transform_predictions(self, mean, std):
-        return self.sigma[0] * mean + self.mu[0], self.sigma[0] * std
+    def inverse_transform(self, X_):
+        X = (self.b - self.a) * X_ + self.a
+        return X
