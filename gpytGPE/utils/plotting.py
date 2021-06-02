5ce318483533a47d1df6cc1df1b56a0a4406e177
Stefano Longobardi <sl18@bioeng306-lap.isd.kcl.ac.uk>
2021-06-02 18:00:22 +0200

    added network and heatmap gsa plotting

--- gpytGPE/utils/plotting.py
+++ gpytGPE/utils/plotting.py
@@ -1,5 +1,6 @@
 import matplotlib.gridspec as grsp
 import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -67,7 +68,7 @@ def interp_col(color, n):
     return lsc
 
 
-def check_distr(A, thre):
+def check_distr_previously(A, threshold):
     mean = []
     std = []
     for a in A.T:
@@ -75,29 +76,57 @@ def check_distr(A, thre):
         std.append(a.std())
     l = []
     for i, (m, s) in enumerate(zip(mean, std)):
-        if m < thre or m - 3 * s < 0:
+        if m < threshold or m - 3 * s < 0:
             l.append(i)
     return l
 
 
-def correct(A, thre):
-    l = check_distr(A, thre)
+def check_distr(A, threshold):
+    lower_quartile = []
+    for a in A.T:
+        lower_quartile.append(np.percentile(a, 25))
+    l = []
+    for i, q1 in enumerate(lower_quartile):
+        if q1 < threshold:
+            l.append(i)
+    return l
+
+
+def correct_index(A, threshold):
+    l = check_distr(A, threshold)
     A[:, l] = np.zeros((A.shape[0], len(l)), dtype=float)
     return A
 
 
-def gsa_box(path, thre, index_i, index_ij, ylab, savefig=False):
-    ST = np.loadtxt(path + "STi.txt", dtype=float)
-    S1 = np.loadtxt(path + "Si.txt", dtype=float)
-    S2 = np.loadtxt(path + "Sij.txt", dtype=float)
+def angle(p, c):
+    [dx, dy] = p - c
 
-    ST = correct(ST, thre)
-    S1 = correct(S1, thre)
-    S2 = correct(S2, thre)
+    if dx == 0:
+        if dy > 0:
+            return 0.5 * np.pi
+        else:
+            return 1.5 * np.pi
+    elif dx > 0:
+        if dy >= 0:
+            return np.arctan(dy / dx)
+        else:
+            return 2.0 * np.pi + np.arctan(dy / dx)
+    elif dx < 0:
+        return np.pi + np.arctan(dy / dx)
+
+
+def gsa_box(ST, S1, S2, index_i, index_ij, ylabel, savepath, correction=None):
+    if correction is not None:
+        ST = correct_index(ST, correction)
+        S1 = correct_index(S1, correction)
+        S2 = correct_index(S2, correction)
 
     df_ST = pd.DataFrame(data=ST, columns=index_i)
     df_S1 = pd.DataFrame(data=S1, columns=index_i)
-    df_S2 = pd.DataFrame(data=S2, columns=index_ij)
+    df_S2 = pd.DataFrame(
+        data=S2,
+        columns=["(" + elem[0] + ", " + elem[1] + ")" for elem in index_ij],
+    )
 
     plt.style.use("seaborn")
     gs = grsp.GridSpec(2, 2)
@@ -130,22 +159,16 @@ def gsa_box(path, thre, index_i, index_ij, ylab, savefig=False):
     )
 
     fig.tight_layout()
-    if savefig:
-        plt.savefig(path + ylab + "_box.pdf", bbox_inches="tight", dpi=1000)
-    else:
-        plt.show()
-    return
+    plt.savefig(savepath + ylabel + "_box.pdf", bbox_inches="tight", dpi=1000)
 
 
-def gsa_donut(path, thre, index_i, ylab, savefig=False):
-    ST = np.loadtxt(path + "STi.txt", dtype=float)
-    S1 = np.loadtxt(path + "Si.txt", dtype=float)
+def gsa_donut(ST, S1, index_i, ylabel, savepath, correction=None):
+    if correction is not None:
+        ST = correct_index(ST, correction)
+        S1 = correct_index(S1, correction)
 
-    ST = correct(ST, thre)
-    S1 = correct(S1, thre)
-
-    S1_mean = np.mean(S1, axis=0)
     ST_mean = np.mean(ST, axis=0)
+    S1_mean = np.mean(S1, axis=0)
 
     sum_s1 = S1_mean.sum()
     sum_st = ST_mean.sum()
@@ -170,7 +193,7 @@ def gsa_donut(path, thre, index_i, ylab, savefig=False):
         wedgeprops=dict(width=0.3, edgecolor="w", linewidth=1),
         normalize=True,
     )
-    axes[0].set_title("S1", fontsize=12)
+    axes[0].set_title("S1", fontsize=12, fontweight="bold")
 
     axes[1].pie(
         x_sti,
@@ -181,17 +204,151 @@ def gsa_donut(path, thre, index_i, ylab, savefig=False):
         wedgeprops=dict(width=0.3, edgecolor="w", linewidth=1),
         normalize=True,
     )
-    axes[1].set_title("ST", fontsize=12)
+    axes[1].set_title("ST", fontsize=12, fontweight="bold")
 
     plt.figlegend(
         wedges, index_i + ["higher-order int."], ncol=5, loc="lower center"
     )
+    plt.savefig(
+        savepath + ylabel + "_donut.pdf", bbox_inches="tight", dpi=1000
+    )
 
-    if savefig:
-        plt.savefig(path + ylab + "_donut.pdf", bbox_inches="tight", dpi=1000)
-    else:
-        plt.show()
-    return
+
+def gsa_heat(ST, S1, index_i, ylabel, savepath, correction=None):
+    if correction is not None:
+        ST = correct_index(ST, correction)
+        S1 = correct_index(S1, correction)
+
+    ST_mean = np.mean(ST, axis=0).reshape(1, -1)
+    S1_mean = np.mean(S1, axis=0).reshape(1, -1)
+
+    height = 9.36111
+    width = 5.91667
+    fig, axes = plt.subplots(1, 2, figsize=(2 * width, 2 * height / 8))
+
+    df = pd.DataFrame(data=S1_mean, index=[ylabel], columns=index_i)
+    h1 = sns.heatmap(
+        df,
+        cmap="rocket_r",
+        vmin=0.0,
+        vmax=1.0,
+        square=True,
+        linewidth=0.1,
+        cbar_kws={"shrink": 0.8},
+        ax=axes[0],
+    )
+    axes[0].set_title("S1", fontsize=12, fontweight="bold")
+    axes[0].tick_params(left=False, bottom=False)
+    h1.set_xticklabels(h1.get_xticklabels(), rotation=45, va="top")
+    h1.set_yticklabels(h1.get_yticklabels(), rotation=0, ha="right")
+
+    df = pd.DataFrame(data=ST_mean, index=[ylabel], columns=index_i)
+    ht = sns.heatmap(
+        df,
+        cmap="rocket_r",
+        vmin=0.0,
+        vmax=1.0,
+        square=True,
+        linewidth=0.1,
+        cbar_kws={"shrink": 0.8},
+        ax=axes[1],
+    )
+    axes[1].set_title("ST", fontsize=12, fontweight="bold")
+    axes[1].tick_params(left=False, bottom=False)
+    ht.set_xticklabels(ht.get_xticklabels(), rotation=45, va="top")
+    ht.set_yticklabels(ht.get_yticklabels(), rotation=0, ha="right")
+
+    plt.savefig(savepath + ylabel + "_heat.pdf", bbox_inches="tight", dpi=1000)
+
+
+def gsa_network(
+    ST, S1, S2, index_i, index_ij, ylabel, savepath, correction=None
+):
+    if correction is not None:
+        ST = correct_index(ST, correction)
+        S1 = correct_index(S1, correction)
+        S2 = correct_index(S2, correction)
+
+    ST_mean = np.mean(ST, axis=0)
+    S1_mean = np.mean(S1, axis=0)
+    S2_mean = np.mean(S2, axis=0)
+
+    maximum = np.max([ST_mean.max(), S1_mean.max(), S2_mean.max()])
+    ST_mean /= maximum
+    S1_mean /= maximum
+    S2_mean /= maximum
+
+    min_size = 0
+    max_size = 200
+    foreground_node_size = [
+        min_size + (max_size - min_size) * k for k in list(S1_mean)
+    ]
+    backgroud_node_size = [
+        min_size + (max_size - min_size) * k for k in list(ST_mean)
+    ]
+    edge_width = [
+        np.sqrt((min_size + (max_size - min_size) * k) / np.pi)
+        for k in list(S2_mean)
+    ]
+
+    Sources = list(list(zip(*index_ij))[0])
+    Targets = list(list(zip(*index_ij))[1])
+    Weights = list(S2_mean)
+
+    G = nx.Graph()
+    for s, t, w in zip(Sources, Targets, Weights):
+        G.add_edges_from([(s, t)], w=w)
+
+    Pos = nx.circular_layout(G)
+
+    c = "blue"
+    colors = interp_col(get_col(c), 5)
+    width = 5.91667
+    fig, axis = plt.subplots(1, 1, figsize=(width, width))
+
+    nx.draw_networkx_nodes(
+        G,
+        Pos,
+        node_size=backgroud_node_size,
+        node_color=len(index_i) * [colors[4]],
+        ax=axis,
+    )
+    nx.draw_networkx_nodes(
+        G,
+        Pos,
+        node_size=foreground_node_size,
+        node_color=len(index_i) * [colors[0]],
+        ax=axis,
+    )
+    nx.draw_networkx_edges(
+        G,
+        Pos,
+        width=edge_width,
+        edge_color=len(index_ij) * [colors[2]],
+        alpha=0.8,
+        ax=axis,
+    )
+
+    center = [0.0, 0.0]
+    radius = 1.0
+
+    names = nx.draw_networkx_labels(
+        G, Pos, font_size=12, font_family="DejaVu Sans", ax=axis
+    )
+    for node, text in names.items():
+        position = (
+            1.2 * radius * np.cos(angle(Pos[node], center)),
+            1.2 * radius * np.sin(angle(Pos[node], center)),
+        )
+        text.set_position(position)
+        text.set_clip_on(False)
+
+    axis.axis("equal")
+    axis.set_axis_off()
+    fig.tight_layout()
+    plt.savefig(
+        savepath + ylabel + "_network.pdf", bbox_inches="tight", dpi=1000
+    )
 
 
 def plot_dataset(Xdata, Ydata, xlabels, ylabels, savepath):
@@ -229,4 +386,3 @@ def plot_dataset(Xdata, Ydata, xlabels, ylabels, savepath):
             axis.set_ylabel(ylabels[i // in_dim])
     plt.suptitle("Sample dimension = {} points".format(sample_dim))
     plt.savefig(savepath + "X_vs_Y.png", bbox_inches="tight", dpi=300)
-    return
